b8849f94a75a3215352dfa13416113d8edf87b84
Hazal Sendogan <hazal.sendogan@maarifa.com.tr>
2020-11-02 16:47:35 +0300

    project created

---
+++ src/root/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {createMuiTheme, withStyles} from "@material-ui/core/styles";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
